c6eb44d6ce1c39df214ae81499cef4f6ff9ef406
ChloeConstan <chloe.constan@oclock.school>
2025-05-05 17:39:27 +0200

    Add JavaScript and API

---
+++ script.js
@@ -0,0 +1,15 @@
+let dernierSigneClique = null;
+
+
+async function afficherHoroscope(signe, url, elementId){
+    const resultat = document.getElementById(elementId);
+    if (dernierSigneClique === `${signe}-${elementId}`) {
+        resultat.innerText = "Ton hosroscope apparaîtra ici ✨";
+        dernierSigneClique = null;
+        return;
+    }
+    const response = await fetch(url);
+    const data = await response.json();
+    resultat.innerText = data[signe];
+    dernierSigneClique = `${signe}-${elementId}`;
+}
